45b9aa21de4012779cc1bc528f99a7c81642e330
Chris Tate <chris@austincodeshop.com>
2021-12-26 03:18:29 -0600

    test

--- functions/api/metis.ts
+++ functions/api/metis.ts
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-export default async function handler(req: any, res: any) {
-  const { method } = req;
-
-  switch (method) {
-    case "GET":
-      const code = req.query.code;
-      const { data } = await axios.get(
-        `https://polis.metis.io/api/v1/oauth2/access_token?app_id=${process.env.APP_ID}&app_key=${process.env.APP_SECRET}&code=${code}`
-      );
-      res.send(data);
-      break;
-    default:
-      res.setHeader("Allow", ["GET"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+export const onRequestGet: PagesFunction = async ({ params, request }) => {
+  if (request.method === "GET") {
+    const { data } = await axios.get(
+      `https://polis.metis.io/api/v1/oauth2/access_token?app_id=${process.env.POLIS_APP_ID}&app_key=${process.env.POLIS_APP_SECRET}&code=${params.code}`
+    );
+    return new Response(JSON.stringify(data), {
+      status: 200,
+    });
   }
-}
+
+  return new Response(JSON.stringify({}), {
+    status: 405,
+  });
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Oauth2Client, HttpClient } from '@metis.io/middleware-client';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import { Oauth2Client, HttpClient } from "@metis.io/middleware-client";
+import { useRouter } from "next/router";
 
 import Layout from "../components/layout";
 
 import "../styles.css";
 
 export default function MyApp({ Component, pageProps }) {
-  const { query } = useRouter();
+  const router = useRouter();
+  console.log(router);
+  const { code } = router.query;
 
   const [httpClient, setHttpClient] = useState<HttpClient>();
-	const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     console.log(user);
@@ -18,48 +20,51 @@ export default function MyApp({ Component, pageProps }) {
   }, [user, httpClient]);
 
   useEffect(() => {
-		const fetchData = async () => {
-			try {
-				if (!query.code) {
-					console.log('error code');
-					return;
-				}
+    if (!router.isReady) {
+      return;
+    }
 
-				const res = await fetch(
-					`${process.env.NEXT_PUBLIC_URL}/api/metis?code=${query.code}`
-				);
+    const fetchData = async () => {
+      try {
+        if (!code) {
+          console.log(code);
+          console.log("error code");
+          return;
+        }
+
+        const res = await fetch(`/api/metis?code=${code}`);
         const resData = await res.json();
 
-				console.log(res);
-				if (res.status === 200 && resData && resData.code === 200) {
-					const accessToken = resData.data.access_token;
-					const refreshToken = resData.data.refresh_token;
-					const expiresIn = resData.data.expires_in;
+        console.log(res);
+        if (res.status === 200 && resData && resData.code === 200) {
+          const accessToken = resData.data.access_token;
+          const refreshToken = resData.data.refresh_token;
+          const expiresIn = resData.data.expires_in;
 
-					const httpClient = new HttpClient(
-						process.env.NEXT_PUBLIC_APP_ID || '',
-						accessToken,
-						refreshToken,
-						expiresIn
-					);
+          const httpClient = new HttpClient(
+            process.env.NEXT_PUBLIC_APP_ID || "",
+            accessToken,
+            refreshToken,
+            expiresIn
+          );
 
-					setHttpClient(httpClient);
+          setHttpClient(httpClient);
 
-					const oauth2Client = new Oauth2Client();
-					setUser(await oauth2Client.getUserInfoAsync(accessToken));
-				} else if (res.status === 200 && resData) {
-					console.log(resData.msg);
-				} else {
-					console.log('code error');
-					console.log(res);
-				}
-			} catch (error) {
-				console.error(error);
-			}
-		};
+          const oauth2Client = new Oauth2Client();
+          setUser(await oauth2Client.getUserInfoAsync(accessToken));
+        } else if (res.status === 200 && resData) {
+          console.log(resData.msg);
+        } else {
+          console.log("code error");
+          console.log(res);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-		fetchData();
-	}, [query.code]);
+    fetchData();
+  }, [router.isReady, code]);
 
   return (
     <Layout>
