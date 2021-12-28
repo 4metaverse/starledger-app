7a607c1df93744a27f8bb7f7aa7937c058f62330
Chris Tate <chris@austincodeshop.com>
2021-12-28 10:46:29 -0600

    uses env var for map url

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
   env: {
     POLIS_APP_ID: process.env.POLIS_APP_ID,
     POLIS_REDIRECT_URL: process.env.POLIS_REDIRECT_URL,
+    STARLEDGER_MAP_URL: process.env. STARLEDGER_MAP_URL,
   },
 };
